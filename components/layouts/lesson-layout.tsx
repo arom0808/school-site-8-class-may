888144b20428c2b7e873d6a5437498c0f8333d87
Roman <47948974+arom0808@users.noreply.github.com>
2022-05-12 12:43:34 +0300

    designing

--- components/layouts/lesson-layout.tsx
+++ components/layouts/lesson-layout.tsx
@@ -1,19 +1,19 @@
-import Footer from "../footer";
 import React from "react";
 import styles from "../../styles/lessons.module.css";
 
 export default function LessonLayout({children}: React.PropsWithChildren<{}>) {
     return (
         <div>
-            <div className={styles.context}>
-                {children}
-            </div>
             <div className={styles.area}>
                 <ul className={styles.circles}>
                     {Array.from({length: 10}, (_, i) => <li key={i}/>)}
                 </ul>
             </div>
-            <Footer/>
+            <div className="w-full top-0 flex px-96">
+                <div className="min-h-screen bg-white/80 shadow-[0_0_40px_0_rgba(255,255,255,0.5)] text-black">
+                    {children}
+                </div>
+            </div>
         </div>
     )
 }
